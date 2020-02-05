1c34f5390f796139e6a817eab3bc7968da604738
AdrianoSantana <ans3@cin.ufpe.br>
2020-02-05 16:00:32 -0300

    Relatório 05/02/2020, Adriano e Juan

---
+++ Cods Esp32/deepSleep/deepSleep.ino
@@ -0,0 +1,60 @@
+/*
+Simple Deep Sleep with Timer Wake Up
+=====================================
+ESP32 offers a deep sleep mode for effective power
+saving as power is an important factor for IoT
+applications. In this mode CPUs, most of the RAM,
+and all the digital peripherals which are clocked
+from APB_CLK are powered off. The only parts of
+the chip which can still be powered on are:
+RTC controller, RTC peripherals ,and RTC memories
+
+This code displays the most basic deep sleep with
+a timer to wake it up and how to store data in
+RTC memory to use it over reboots
+*/
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Estou acordado ");
+  delay(5000); 
+
+  //++bootCount;
+  //Serial.println("Boot number: " + String(bootCount));
+
+  print_wakeup_reason();
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  //Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  //" Seconds");
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+  
+}
+
+void loop(){
+  //This is not going to be called
+}
